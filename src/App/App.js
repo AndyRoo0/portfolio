592c1e048c25496504abc363a7f7e9b2216015b1
Andrew <andrew@eseeds.com>
2023-02-17 22:09:24 +0000

    Added navbar & animation

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import './App.css';
 import portrait from '../images/portrait.png';
+import arrow from '../images/arrow.svg';
 
 function App() {
   return (
     <div>
-      <nav className='nav'>
-        <p>Nav</p>
-      </nav>
+      <ul className='nav'>
+        <li>
+          <a href='default.asp'>Resume</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#projects'>Projects</a>
+        </li>
+        <li>
+          <a href='about.asp'>Contact Me</a>
+        </li>
+      </ul>
+
       <div className='welcome'>
         <h1>Andrew Rohling</h1>
         <p>Full-Stack Web Developer.</p>
       </div>
-      <section className='waves'></section>
-      <div className='whoami'>
+      <section className='waves'>
+        <img className='arrow' src={arrow} alt='An arrow' height='50vh' />
+      </section>
+      <div className='whoami' id='about'>
         <p>Hey there, welcome to my little corner of the web!</p>
         <h1>Who am I?</h1>
         <p>I am me</p>
-        <img src={portrait} alt='Me' height='100vh' />
+        <img className='portrait' src={portrait} alt='Me' height='100vh' />
       </div>
-      <div className='projects'>
+      <div className='projects' id='projects'>
         <h1>What have I been working on?</h1>
         <p>Cool stuff here</p>
       </div>
