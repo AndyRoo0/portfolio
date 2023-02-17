c1c3107823516d48aa27a47349c2265018fd4cb8
Andrew <andrew@eseeds.com>
2023-02-17 20:02:38 +0000

    playing around with picture locations

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import portrait from '../images/portrait.png';
 
 function App() {
   return (
@@ -9,13 +10,14 @@ function App() {
       </nav>
       <div className='welcome'>
         <h1>Andrew Rohling</h1>
-        <p>A Full-Stack Web Developer.</p>
+        <p>Full-Stack Web Developer.</p>
       </div>
       <section className='waves'></section>
       <div className='whoami'>
         <p>Hey there, welcome to my little corner of the web!</p>
         <h1>Who am I?</h1>
         <p>I am me</p>
+        <img src={portrait} alt='Me' height='100vh' />
       </div>
       <div className='projects'>
         <h1>What have I been working on?</h1>
