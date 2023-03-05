63d55a69cb1021802d37ee8c605d110073b4bf19
Andrew <andrew@eseeds.com>
2023-03-05 14:52:56 +0000

    Refactored project page to use cards & props, to make it easy to add more projects

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function ProjectCard() {
-  return <div>ProjectCard</div>;
+function ProjectCard({ details }) {
+  return (
+    <div>
+      <div className='project-table'>
+        <div className='col1'>
+          <h2 className='proj-title'>{details.title}</h2>
+          <a
+            href={details.link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='proj-link'
+          >
+            View the deployed project here.
+          </a>
+
+          <p className='proj-desc'>{details.desc}</p>
+          <p>Click on the image to learn more.</p>
+        </div>
+        <div className='proj-img'>
+          <Link to={details.page}>
+            <img src={details.img} alt={details.alt}></img>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import indygo from '../../resources/IndyGo image.png';
-import { Link } from 'react-router-dom';
+import ProjectCard from './ProjectCard';
 
 function Projects() {
   return (
@@ -9,34 +9,16 @@ function Projects() {
       <div className='line'>
         <h1>What have I been working on?</h1>
       </div>
-      <div className='project-table'>
-        <div className='col1'>
-          <h2 className='indy-title'>IndyGo</h2>
-          <a
-            href='https://indygo.netlify.app/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='indy-link'
-          >
-            View the deployed project here.
-          </a>
-
-          <p className='indy-desc'>
-            IndyGo is a mobile-focused application that allows local independent
-            businesses to fight back against economic pressures by offering live
-            deals to consumers within a walkable distance. Consumers can use
-            IndyGo to get active and shop local with a map of independent
-            businesses in their area, and can scroll through a carousel of deals
-            available to them.
-          </p>
-          <p>Click on the image to learn more.</p>
-        </div>
-        <div className='indy-img'>
-          <Link to='/indygo'>
-            <img src={indygo} alt='IndyGo Home Page'></img>
-          </Link>
-        </div>
-      </div>
+      <ProjectCard
+        details={{
+          title: 'IndyGo',
+          link: 'https://indygo.netlify.app/',
+          desc: 'IndyGo is a mobile-focused application that allows local independent businesses to fight back against economic pressures by offering live deals to consumers within a walkable distance. Consumers can use IndyGo to get active and shop local with a map of independent businesses in their area, and can scroll through a carousel of deals available to them.',
+          img: indygo,
+          alt: 'IndyGo Home Page',
+          page: '/indygo',
+        }}
+      />
     </div>
   );
 }
