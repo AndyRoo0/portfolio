ea7c702fded1b1a742f7b6073cc18a5427e204ba
Andrew <andrew@eseeds.com>
2023-03-08 13:01:35 +0000

    Added points to remember on IndyGo page

--- src/Pages/ProjectPages/IndyGo.js
+++ src/Pages/ProjectPages/IndyGo.js
@@ -15,13 +15,22 @@ function IndyGo() {
         View IndyGo on GitHub here
       </a>
       <p>
-        This project started with the brief: 'Build an app to solve a problem'
-      </p>
-      <p>
+        This project started with the brief: 'Build an app to solve a problem'.
         As a team of 6, with a time frame on just 1 month, we started
         brainstorming ideas.
       </p>
-      <p>This page is still under construction! Check back soon...</p>
+      <p>
+        This page is still under construction! It will be updated to include our
+        full process including that in the list below. Check back soon...
+      </p>
+      <ul>
+        <li>Disney Ideation & how we came up with the initial idea</li>
+        <li>Market Research</li>
+        <li>Miro/Trello organisation</li>
+        <li>Tech Stack choices & discussion</li>
+        <li>Low & High fidelity wireframes & Figma Design</li>
+        <li>Pull Requests & CI/CD workflow</li>
+      </ul>
       <Link to='/'>
         <button>Back to home</button>
       </Link>
