228cdc809baf1d947dd83727d0f9cecf990c060f
Andrew <andrew@eseeds.com>
2023-03-10 16:16:22 +0000

    Commented code on the dinosaur project

--- src/Pages/ProjectPages/DinosaurFacts/DinoFacts.js
+++ src/Pages/ProjectPages/DinosaurFacts/DinoFacts.js
@@ -7,93 +7,108 @@ import Rock3 from './Rocks/Rock3.png';
 import Bone from './Rocks/bone.png';
 import GitBone from './Rocks/gitBone.png';
 
+/**
+ * A React list item component
+ * @param {*} props - A Name, a Description, a Url
+ * @returns A 'dinosaur' list item with a name, description & url to populate the history list.
+ */
 function Dinosaur(props) {
-  console.log('props', props);
   return (
     <li className='listitem'>
-      <p className='listp listname'>{props.name}</p>
+      <p className='listp'>{props.name}</p> {/*Displays the dinosaur name*/}
       <p className='listp listdesc'>{props.description}</p>
+      {/*Displays the dinosaur description*/}
       <a
         href={props.url}
         target='_blank'
         rel='noopener noreferrer'
-        className='listp listlink'
+        className='listp'
       >
-        Link
+        Link {/*Displays the dinosaurs Wikipedia link. Opens in new tab*/}
       </a>
     </li>
   );
 }
 
 function DinoFacts() {
-  const [dinoName, setDinoName] = useState('');
-  const [dinoDesc, setDinoDesc] = useState('');
-  const [dinoUrl, setDinoUrl] = useState('');
-  const [clicks, setClicks] = useState(0);
-  const [imageUrl, setImageUrl] = useState(Rock1);
+  const [dinoName, setDinoName] = useState(''); //State used to capture the dinosaur name, to be displayed
+  const [dinoDesc, setDinoDesc] = useState(''); //Used to capture the description
+  const [dinoUrl, setDinoUrl] = useState(''); //Used to save the wikipedia link
+  const [clicks, setClicks] = useState(0); // State to keep track of the number of clicks before a new request is made
+  const [imageUrl, setImageUrl] = useState(Rock1); //State used to keep track of the rock image changing as it is clicked
   const [dinosaurs, setDinosaurs] = useState([
     { Name: 'Name', Description: 'Description', Url: '' },
-  ]);
+  ]); //Array state that forms the history list of previous dinosaurs. This pre-populated data actually acts as the column headers when displayed.
 
-  const handleClick = async () => {
+  /**
+   * Responsible for changing the rock image on click, adding and resetting the click counter, calling the api & assigning its data to the dinosaurs array.
+   */
+  async function handleClick() {
     switch (clicks) {
       case 0:
-        setImageUrl(Rock2);
-        setClicks(clicks + 1);
+        setImageUrl(Rock2); //Changes rock image to the cracked image
+        setClicks(clicks + 1); //Adds one to the click count state
         break;
 
       case 1:
-        setImageUrl(Rock3);
-        setClicks(clicks + 1);
+        setImageUrl(Rock3); //Changes rock image to the image with a bone sticking out
+        setClicks(clicks + 1); //Adds one to the click count state
         break;
 
       case 2:
-        setClicks(0);
-        setImageUrl(Rock1);
+        setClicks(0); //Resets the click count state
+        setImageUrl(Rock1); //Changes rock image back to the unblemished specimen
         const response = await fetch(
           `https://dinosaur-facts-api.shultzlab.com/dinosaurs/random`
-        );
-        const data = await response.json();
-        console.log(data);
-        setDinoName(data.Name);
-        setDinoDesc(data.Description);
-        setDinoUrl(`https://en.wikipedia.org/wiki/${data.Name}`);
+        ); //Calls API
+        const data = await response.json(); //Parses JSON response to a JS object
+        setDinoName(data.Name); // Sets DinoName state to be received data
+        setDinoDesc(data.Description); // Sets DinoDesc state to be received data
+        setDinoUrl(`https://en.wikipedia.org/wiki/${data.Name}`); // Creates the wikipedia link (not actually supplied from the API, yet to find a link that doesn't work though!) Updates the DinoUrl state.
         const dinodata = {
           Name: data.Name,
           Description: data.Description,
           Url: `https://en.wikipedia.org/wiki/${data.Name}`,
-        };
-        setDinosaurs((previous) => [...previous, dinodata]);
+        }; //Creates the object with the key value pairs needed for the list item.
+        setDinosaurs((previous) => [...previous, dinodata]); //Adds the new dinosaur data to the array to be mapped later.
         break;
 
       default:
         break;
     }
-  };
+  }
 
   return (
     <div className='dinocontent'>
       <nav className='dinoNav'>
+        {/* Navbar containing Back and Github link bones */}
         <Link to='/' className='back'>
+          {/* React-Router-Dom link to website homepage */}
           <img className='bone' src={Bone} alt='A bone, links to homepage' />
         </Link>
-        <a href='https://github.com/AndyRoo0/portfolio/tree/main/src/Pages/ProjectPages/DinosaurFacts'>
-          <img className='bone' src={GitBone} alt='A bone, links to homepage' />
+        <a
+          href='https://github.com/AndyRoo0/portfolio/tree/main/src/Pages/ProjectPages/DinosaurFacts'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img className='bone' src={GitBone} alt='A bone, links to github' />
         </a>
       </nav>
       <section className='dinopage'>
+        {/* Main page content */}
         <img
           src={imageUrl}
           className='rock'
           alt='A rock, contains dinosaur fossils'
           onClick={handleClick}
         />
+        {/* React conditional on all tags, rendering to display instructions if there has been no API call */}
         {dinoName.length > 0 ? (
           <h2>{dinoName}</h2>
         ) : (
           <h2>Click on the rock to find a dinosaur!</h2>
         )}
-        {dinoUrl.length > 0 && <h3>{dinoDesc}</h3>}
+        {dinoDesc.length > 0 && <h3>{dinoDesc}</h3>}
         {dinoUrl.length > 0 && (
           <a
             href={dinoUrl}
@@ -105,9 +120,11 @@ function DinoFacts() {
           </a>
         )}
       </section>
+      {/* Previous dinosaur history. Does not display unless API call has been made. */}
       {dinoName.length > 0 && (
         <section className='history'>
           <ul className='list'>
+            {/* Iterates through dinosaurs array, forming a 'Dinosaur' react component for each item. Passes down the Name, Description & Url as props*/}
             {dinosaurs.map((dinosaur) => (
               <Dinosaur
                 name={dinosaur.Name}
--- src/Pages/ProjectPages/DinosaurFacts/ReadMe.md
+++ src/Pages/ProjectPages/DinosaurFacts/ReadMe.md
@@ -1,1 +1,11 @@
-Test Readme
+## Dinosaurs!
+
+### How to:
+
+Click/Tap on the rock to mine it. You'll likely find a fossil, they seem pretty common around here. You'll see it's name, a little tidbit about it, and a link to Wikipedia to find out more. Previous dinosaurs you have discovered will populate the list below. Thats it, go find some cool dinos!
+
+---
+
+This project was built in about a day and a half, as a bit of fun & to start to get more API work on my portfolio.
+
+Built with React & CSS.
